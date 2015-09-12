02519a08da6f66c081cad51ccd971fcd7181be48
ralf-o <ralf-o@users.noreply.github.com>
2015-09-12 17:28:15 +0200

    Bunch of updates

--- demo/blingbling/demo/todomvc.cljs
+++ demo/blingbling/demo/todomvc.cljs
@@ -2,32 +2,56 @@
   (:require [blingbling.core :as bling]
             [cljs.reader :as reader]))
 
+(comment
+  TODOS
+  =====
+
+  !!!Increase code quality heavily!!!
+
+  Add function descriptions.
+
+  Comment the code.
+
+  Use records where useful, especially for the events.
+
+  Split in smaller functions, especially the views.
+
+  Find a better solution for those "dispatch" and "publish" callbacks.
+  (=> bligbling.core)
+
+  Find some way of "dependency injection" for the local storage stuff (reader monad???)
+  )
+
+;; Just to active bling-bling's "debug mode"
+;; (set! bling/debug-all true)
+
 (def local-storage-key "todomvc.blingbling.state")
 
 (def filters
   {:all       (fn [] true)
-   :active    (complement :completed?)
-   :completed :completed?})
+   :active    (complement :completed)
+   :completed :completed})
 
 
-(defn todomvc-controller [event state dispatch publish]
+(defn todomvc-controller [state event]
   (let [[event-key arg arg2] event
         find-index-by (fn [coll id f] (first (keep-indexed #(if (= id (f %2)) %1) coll))) ;; TODO
         inc-next-id #(update % :next-id inc)]
     ;; (print (str "-----> "  arg " : " (:todos state) "|" (keep-indexed #(if (= arg (:id %2)) %1) (:todos state)) "|")); (find-index-by-id (:todos state) 2)))
+       (print event)
     (let [new-state (case event-key
                       :set-filter (assoc state :active-filter arg)
 
                       :clear-completed-todos (update state :todos
-                                                     #(vec (filter (complement :completed?) %)))
+                                                     #(vec (filter (complement :completed) %)))
 
                       :set-todo-completed (assoc-in state
-                                                    [:todos (+ 0 (find-index-by-id (:todos state) arg)) :completed?] arg2) ;; TODO!!!!
+                                                    [:todos (+ 0 (find-index-by-id (:todos state) arg)) :completed] arg2) ;; TODO!!!!
 
                       :set-todo-completed-for-all (update state :todos
                                                           #(mapv
                                                             (fn [todo]
-                                                              (assoc todo :completed? arg)) %))
+                                                              (assoc todo :completed arg)) %))
 
                       :add-todo (inc-next-id (update state :todos
                                                      #(conj % {:id (:next-id state) :text arg :completed false})))
@@ -47,9 +71,9 @@
         active-filter (:active-filter state)
         visible-todos (filter (active-filter filters) todos)
         total-todo-count (count todos)
-        active-todo-count (->> todos (filter (complement :completed?)) count)]
+        active-todo-count (->> todos (filter (complement :completed)) count)]
 
-    [:section.todoapp
+    [:section.todoapp #_(print visible-todos)
      [:header.header
       [:h1 "todos"]
       [:input.new-todo {:placeholder "What needs to be done?"
@@ -66,8 +90,7 @@
 
       [:label {:for "toggle-all"}
        "Mark all as complete"]
-      [:ul.todo-list (map-indexed
-                       (fn [index todo] ^{:key (:id todo)} [todomvc-item {:todo todo :=> dispatch}])
+      [:ul.todo-list (map-indexed (fn [index todo] ^{:key (:id todo)} [todomvc-item {:todo todo :=> dispatch}])
                        visible-todos)]]
 
      [:footer.footer
@@ -91,28 +114,28 @@
        [:p " Created by " [:a {:href "http://www.google.com"} "Ralf"]]
        [:p " Part of " [:a {:href "http://todomvc.com"} "TodoMVC"]]]]]))
 
-(defn- todomvc-initial-state-provider [] (print 4444)
-  (let [state (.getItem js/localStorage local-storage-key)]
-    (if (nil? state)
-      {:todos [], :active-filter :all, :next-id 0}
-      (reader/read-string state))))
+(defn- todomvc-initial-state-provider [] (
+                                           (let [state (.getItem js/localStorage local-storage-key)]
+                                             (if (nil? state)
+                                               {:todos [], :active-filter :all, :next-id 0}
+                                               (reader/read-string state)))))
 
 
-(def todomvc (bling/create-component-class todomvc-view todomvc-controller todomvc-initial-state-provider nil))
+(def todomvc (bling/create-component-class todomvc-view todomvc-controller {:todos [] :active-filter :all :next-id 0} nil))
 
 ;; --------------------------------------
 
 (defn todomvc-item-view [props state dispatch]
   (let [todo (:todo props)
         id (:id todo)
-        class (str (if (:completed? todo) "completed" "active") (if (:in-edit-mode? state) "-editing editing"))
+        class (str (if (:completed todo) "completed" "active") (if (:in-edit-mode? state) "-editing editing"))
         toggle-edit-mode #(dispatch [:toggle-edit-mode])
         set-todo-text #(dispatch [:set-todo-text id (.-target.value %)])]
 
     [:li {:key id :class class}
      [:div.view
       [:input.toggle {:type      "checkbox"
-                      :checked   (:completed? todo)
+                      :checked   (:completed todo)
                       :on-change #(dispatch [:set-todo-completed id (.-checked (.-target %))])}]
 
       [:label {:on-double-click toggle-edit-mode} (:text todo)]
@@ -123,13 +146,12 @@
                    :on-key-down #(if (= (.-keyCode %) 13) (toggle-edit-mode))
                    :on-blur     toggle-edit-mode}]]))
 
-(defn todomvc-item-controller [event state dispatch publish]
+(defn todomvc-item-controller [state event]
   (let [[event-key arg] event]
     (case event-key
-      :toggle-edit-mode (update state :in-edit-mode? #(not %))
+      :toggle-edit-mode [(update state :in-edit-mode? #(not %)) nil]
+      [state event])))
 
-      ;; publish all other events to parent component
-      (publish event))))
 
 (def todomvc-item (bling/create-component-class todomvc-item-view todomvc-item-controller {:in-edit-mode? false} nil))
 
--- src/blingbling/core.cljs
+++ src/blingbling/core.cljs
@@ -1,61 +1,109 @@
 (ns blingbling.core
   (:require [reagent.core :as r]))
 
-(defn create-component-class
-  [view controller initial-state mixins]
-  {:pre
-   [(fn? view)
-    ((or nil? fn?) controller)
-    ((or nil? associative?) mixins)]}
+#_(comment
+    TODOS
+    =====
 
-  (fn [& args]
-    (let
-      [children (if (associative? (first args)) (rest args) args)
-       state (if (fn? initial-state) (initial-state) initial-state)
-       state-atom (r/atom state)
-       dispatcher-factory (obtain-dispatcher-factory controller state-atom)]
-      (print stat)
-      (fn [props]
-        (vec
-          (concat
-            (view props @state-atom (dispatcher-factory props))
-            children))))))
+    Increase code quality.
+
+    Add function descriptions.
+
+    Comment the code.
+
+    Handle argument "mixins" in function "create-component-class" .
+
+    Decide whether we should use some proper protocol
+    stead of the "create-component-class" with lots of arguments.
+
+    Asyncronous controller logic is not handled at all yet (see also next point)
+
+    Make the controller method REALLY pure, even if the controller
+    will call some IO stuff (access to web storage, ajax requests etc.)
+    => some monadic approach?
+
+    Find a better solution for those "dispatch" and "publish" callbacks.
+    )
+
+(def *debug-all* false)
+
+(defn create-component-class
+      [view controller initial-state mixins]
+      {:pre
+       [(fn? view)
+        ((or nil? fn?) controller)
+        ((or nil? associative?) mixins)]}
+
+      (fn [& args]
+          (let
+            [children (if (associative? (first args)) (rest args) args)
+             state (if (fn? initial-state) (initial-state) initial-state)
+             state-atom (r/atom state)
+             dispatcher-factory (obtain-dispatcher-factory controller state-atom)]
+
+            (fn [props]
+                (vec
+                  (concat
+                    (view props @state-atom (dispatcher-factory props))
+                    children))))))
 
 
 (defn mount-component [component target]
-  (let [dom-element (if (string? target) (.getElementById js/document target) target)]
-    (r/render-component component dom-element)))
+      (let [dom-element (if (string? target) (.getElementById js/document target) target)]
+           (r/render-component component dom-element)))
 
 ;; --- private --------------------------------------------
 
 (defn- obtain-publisher [props]
-  (let [publish (:=> props)]
-    (if (fn? publish) publish (fn [_] nil))))
+       (let [publish (:=> props)]
+            (if (fn? publish) publish (fn [_] nil))))
 
 (defn- obtain-dispatcher-factory [controller state-atom]
-  (fn [props]
-    (if
-      (nil? controller)
-      (fn [_] nil)
-      (let [publish (obtain-publisher props)
-            ;; This is just for debugging purposes (activates the "debug mode")
-            debug (:!!! props)
-            dispatch (fn [event]
-                       (let [new-state (controller event @state-atom dispatch publish)]
-                         ;; Prints out a debug information if you are in debug mode
-                         (if debug
-                           (print (str "\n\n=== STATE TRANSITION ========="
-                                       "\n--- old state ----------------\n"
-                                       @state-atom
-                                       "\n\n--- new state ----------------\n"
-                                       (if (= @state-atom new-state)
-                                         "(no changes)"
-                                         new-state)
-                                       "\n\n--- event --------------------\n"
-                                       event
-                                       (when (not= debug true)
-                                         (str "\n\n--- info ---------------------\n"
-                                              debug "\n"))
-                                       "===============================\n\n")))
-                         (reset! state-atom new-state)))]
-        dispatch))))
+       (fn [props]
+           (if
+             (nil? controller)
+             (fn [_] nil)
+             (let [publish (obtain-publisher props)
+                   ;; This is just for debugging purposes (activates the "debug mode")
+                   debug (:!!! props)
+                   dispatch (fn [event]
+
+                                (let [old-state @state-atom
+                                      controller-result (controller @state-atom event)
+                                      new-state (retrieve-new-state controller-result)
+                                      new-events (retrieve-new-events controller-result)]
+                                     ;; Prints out a debug information if we are in debug mode
+                                     (if (or debug *debug-all*)
+                                       (print (str "\n\n=== STATE TRANSITION ========="
+                                                   "\n--- old state ----------------\n"
+                                                   old-state
+                                                   "\n\n--- new state ----------------\n"
+                                                   (if (= old-state new-state)
+                                                     "(no changes)"
+                                                     new-state)
+                                                   "\n\n--- event --------------------\n"
+                                                   event
+                                                   (when (and (not-empty debug) (not= debug true))
+                                                         (str "\n\n--- info ---------------------\n"
+                                                              debug))
+                                                   "\n===============================\n\n")))
+                                     (reset! state-atom new-state)
+                                     (publish-events new-events publish)))]
+                  dispatch))))
+
+(defn- retrieve-new-state [controller-result]
+       (if (vector? controller-result)
+         (get controller-result 0)
+         controller-result))
+
+(defn- retrieve-new-events [controller-result]
+       (if (vector? controller-result)
+         (get controller-result 1)
+         controller-result))
+
+(defn- publish-events [events publish]
+       (let [seq (if (seq? events)
+                   events
+                   [events])]
+            (doseq [event seq]
+                   (publish event))))
